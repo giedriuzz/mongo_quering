594ed2ea887370a38b66550f77a52d2382aefb40
kraujinis <kraujinis@gmail.com>
2023-05-29 18:25:27 +0300

    deleleted quering.py

--- main.py
+++ main.py
@@ -28,19 +28,6 @@ class TaskManager:
     def get_all_tasks(self) -> List[Dict[str, Any]]:
         return list(self.collection.find())
 
-    # def get_task(self, task_name: str) -> Dict[str, Any]:
-    #     return list(self.collection.find({​"task_name": task_name}​))
-
-    # def update_task(self, task_name: str, task_updates: Dict[str, Any]) -> bool:
-    #     result = self.collection.update_one(
-    #         {​"task_name": task_name}​, {​"$set": task_updates}​
-    #     )
-    #     return result.modified_count > 0
-
-    # def delete_task(self, task_name: str) -> bool:
-    #     result = self.collection.delete_one({​"task_name": task_name}​)
-    #     return result.deleted_count > 0
-
     def filter_by_equals(self, field_name: str, value: str) -> List[dict]:
         """$eq It will match the values that are equal to a specified value."""
         query = {field_name: {"$eq": value}}
